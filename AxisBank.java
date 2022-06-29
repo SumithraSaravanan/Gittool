e3c915ec36cf5707db30c6ed5ccf9436470f1fe1
SumithraSaravanan <sumi29051996@gmail.com>
2022-06-29 15:47:48 +0530

    master work is done

---
+++ AxisBank.java
@@ -0,0 +1,11 @@
+package org.bank;
+
+public class AxisBank {
+	
+	public void deposit() {
+		System.out.println("The minimum deposit amount is 10K");
+
+
+	}
+
+}
---
+++ BankInfo.java
@@ -0,0 +1,24 @@
+package org.bank;
+
+public class BankInfo extends AxisBank {
+	
+	private void saving() {
+		System.out.println("This is saving Account");
+
+	}
+	
+	private void fixed() {
+		System.out.println("This is fixed Account");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		BankInfo b=new BankInfo();
+		b.saving();
+		b.fixed();
+		b.deposit();
+		
+	}
+
+}
